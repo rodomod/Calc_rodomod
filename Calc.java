f76ecf73f4d267c246cde98cd49fe4461da40b8e
rodomod <odministrator@hotmail.com>
2019-11-02 18:43:34 +0400

    winContent

--- Calc.java
+++ Calc.java
@@ -6,18 +6,20 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
+import javax.swing.SwingConstants;
 
 public class Calc {
-    JPanel winContent,p1;
+    JPanel p0,p01;
     JTextField display;
-    JButton b0,b1,b2,b3,b4,b5,b6,b7,b8,b9,bPtn,bEquals,bSum,bSub,bMul,bDiv,bOps,bBack,bse,bSqrt,bPow;
+    JButton b0,b1,b2,b3,b4,b5,b6,b7,b8,b9,bPtn,bRezult,bSum,bSub,bMul,bDiv,bOps,bBack,bSe,bSqrt,bPow;
     
-Calc(){
-        winContent = new JPanel();
+Calc() {
+        p0 = new JPanel();
         BorderLayout bl=new BorderLayout();
-        winContent.setLayout(bl);
+        p0.setLayout(bl);
         display=new JTextField(30);
-        winContent.add("North",display);
+        display.setHorizontalAlignment(SwingConstants.RIGHT);
+        p0.add("North",display);
         b0=new JButton("0");
 		b1=new JButton("1");
 		b2=new JButton("2");
@@ -35,98 +37,123 @@ Calc(){
 		bDiv=new JButton("/");
         bOps=new JButton("-/+");
 		bBack=new JButton("C");
-		bse=new JButton("CE");
+		bSe=new JButton("CE");
 		bSqrt=new JButton("sqrt");
 		bPow=new JButton("pow^3");
-        p1=new JPanel();
+        p01=new JPanel();
         GridLayout gl=new GridLayout(4,20);
-        p1.setLayout(gl);
+        p01.setLayout(gl);
+        p01.add(b0);
+        p01.add(b1);
+        p01.add(b2);
+        p01.add(b3);
+        p01.add(b4);
+        p01.add(b5);
+        p01.add(b6);
+        p01.add(b7);
+        p01.add(b8);
+        p01.add(b9);
+        p01.add(bPtn);
+        p01.add(bSum);
+        p01.add(bSub);
+        p01.add(bMul);
+        p01.add(bDiv);
+        p01.add(bOps);
+        p01.add(bBack);
+        p01.add(bSe);
+        p01.add(bSqrt);
+        p01.add(bPow);
+        p0.add("Center",p01);
         
-        p1.add(b0);p1.add(b1);p1.add(b2);p1.add(b3);p1.add(b4);p1.add(b5);
-        p1.add(b6);p1.add(b7);p1.add(b8);p1.add(b9);
-        p1.add(bPtn);p1.add(bSum);p1.add(bSub);p1.add(bMul);
-        p1.add(bDiv);p1.add(bOps);p1.add(bBack);p1.add(bse);p1.add(bSqrt);p1.add(bPow);
-        winContent.add("Center",p1);
-        
-        bEquals=new JButton("=");
-        winContent.add("South",bEquals);
+        bRezult=new JButton("=");
+        p0.add("South",bRezult);
         
         JFrame frame=new JFrame("Calc_ENG");
-        frame.setContentPane(winContent);
+        frame.setContentPane(p0);
         frame.pack();
         frame.setVisible(true);
         frame.setLocationRelativeTo(null);
         frame.setResizable(false);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         
-        CalcEngine calcen=new CalcEngine(this);
-        bPow.addActionListener(calcen);bSqrt.addActionListener(calcen);bse.addActionListener(calcen);
-        bBack.addActionListener(calcen);bOps.addActionListener(calcen);
-        bEquals.addActionListener(calcen);bSum.addActionListener(calcen);
-        bSub.addActionListener(calcen);bMul.addActionListener(calcen);
-        bDiv.addActionListener(calcen);bPtn.addActionListener(calcen);
-        b0.addActionListener(calcen);b1.addActionListener(calcen);
-        b2.addActionListener(calcen);b3.addActionListener(calcen);
-        b4.addActionListener(calcen);b5.addActionListener(calcen);
-        b6.addActionListener(calcen);b7.addActionListener(calcen);
-        b8.addActionListener(calcen);b9.addActionListener(calcen);
-                    }
-public class CalcEngine implements ActionListener {
-    Calc parent;
-    char selAction=' ';  
-    double curRes=0;
-  CalcEngine(Calc parent){this.parent=parent;}
-    @Override
- public void actionPerformed(ActionEvent e){
-        JButton clickedButton=(JButton)e.getSource();
-        String dspText=parent.display.getText();
-        double dspValue=0; 
-        if(!"".equals(dspText)){dspValue=Double.parseDouble(dspText);}
-        Object src=e.getSource();
-             if(src==parent.bSum){selAction='+';curRes=dspValue;parent.display.setText("");}
-        else if(src==parent.bSub){selAction='-';curRes=dspValue;parent.display.setText("");}
-        else if(src==parent.bMul){selAction='*';curRes=dspValue;parent.display.setText("");}
-        else if(src==parent.bDiv){selAction='/';curRes=dspValue;parent.display.setText("");}
-else if(src==parent.bEquals){
-            if(selAction=='+'){curRes+=dspValue;parent.display.setText(""+curRes);}
-            if(selAction=='-'){curRes-=dspValue;parent.display.setText(""+curRes);}
-            if(selAction=='*'){curRes*=dspValue;parent.display.setText(""+curRes);}
-            if(selAction=='/'){curRes/=dspValue;parent.display.setText(""+curRes);}
+        Eng en=new Eng(this);
+        b0.addActionListener(en);
+        b1.addActionListener(en);
+        b2.addActionListener(en);
+        b3.addActionListener(en);
+        b4.addActionListener(en);
+        b5.addActionListener(en);
+        b6.addActionListener(en);
+        b7.addActionListener(en);
+        b8.addActionListener(en);
+        b9.addActionListener(en);
+        bPow.addActionListener(en);
+        bSqrt.addActionListener(en);
+        bSe.addActionListener(en);
+        bBack.addActionListener(en);
+        bOps.addActionListener(en);
+        bSum.addActionListener(en);
+        bSub.addActionListener(en);
+        bMul.addActionListener(en);
+        bDiv.addActionListener(en);
+        bPtn.addActionListener(en); 
+        bRezult.addActionListener(en);
                    }
-else  if(src==parent.bPtn){
-      String ptn=parent.display.getText();
-      if(!ptn.contains("."))parent.display.setText(parent.display.getText()+".");
+public class Eng implements ActionListener {
+    Calc rodomod;
+    char cA=' ';  
+    double cR=0;
+Eng(Calc rodomod){this.rodomod=rodomod;}
+    @Override
+public void actionPerformed(ActionEvent e) {
+        JButton ib=(JButton)e.getSource();
+        String dt=rodomod.display.getText();
+        double dV=0; 
+        if(!"".equals(dt)){dV=Double.parseDouble(dt);}
+        Object src=e.getSource();
+             if(src==rodomod.bSum){cA='+';cR=dV;rodomod.display.setText("");}
+        else if(src==rodomod.bSub){cA='-';cR=dV;rodomod.display.setText("");}
+        else if(src==rodomod.bMul){cA='*';cR=dV;rodomod.display.setText("");}
+        else if(src==rodomod.bDiv){cA='/';cR=dV;rodomod.display.setText("");}
+else if(src==rodomod.bRezult){
+            if(cA=='+'){cR+=dV;rodomod.display.setText(""+cR);}
+            if(cA=='-'){cR-=dV;rodomod.display.setText(""+cR);}
+            if(cA=='*'){cR*=dV;rodomod.display.setText(""+cR);}
+            if(cA=='/'){cR/=dV;rodomod.display.setText(""+cR);}
+                   }
+else  if(src==rodomod.bPtn){
+      String pnt=rodomod.display.getText();
+      if(!pnt.contains("."))rodomod.display.setText(rodomod.display.getText()+".");
                       }
-else if(src==parent.bOps){
-      double ops=Double.parseDouble(String.valueOf(parent.display.getText()));
+else if(src==rodomod.bOps){
+      double ops=Double.parseDouble(String.valueOf(rodomod.display.getText()));
       ops*=(-1.D);
-      parent.display.setText(String.valueOf(ops));
+      rodomod.display.setText(String.valueOf(ops));
                       }
-else if(src==parent.bBack){
-      String temp=parent.display.getText();
-      parent.display.setText(temp.substring(0,temp.length()-1));
+else if(src==rodomod.bBack){
+      String temp=rodomod.display.getText();
+      rodomod.display.setText(temp.substring(0,temp.length()-1));
                       }
-else if(src==parent.bse){
-	parent.display.setText("");
+else if(src==rodomod.bSe){
+	rodomod.display.setText("");
                       }
-else if(src==parent.bSqrt){
-      double fm=Double.parseDouble(parent.display.getText());
-      double sm=Double.parseDouble(parent.display.getText());
-      String sqrt=parent.display.getText();
-      if(sqrt.indexOf("")==0)parent.display.setText(Math.sqrt(sm*fm/sm)+"");
+else if(src==rodomod.bSqrt){
+      double fm=Double.parseDouble(rodomod.display.getText());
+      double sm=Double.parseDouble(rodomod.display.getText());
+      String sqrt=rodomod.display.getText();
+      if(sqrt.indexOf("")==0)rodomod.display.setText(Math.sqrt(sm*fm/sm)+"");
                       }
-else if(src==parent.bPow){
-      double fs=Double.parseDouble(parent.display.getText());
-      String pow=parent.display.getText();
-      if(pow.indexOf("")==0){parent.display.setText(Math.pow(fs,3)+"");}
+else if(src==rodomod.bPow){
+      double fs=Double.parseDouble(rodomod.display.getText());
+      String pow=rodomod.display.getText();
+      if(pow.indexOf("")==0){rodomod.display.setText(Math.pow(fs,3)+"");}
                       }
-else {String  clickedButtonLabel=clickedButton.getText();
-          parent.display.setText(dspText+clickedButtonLabel);}
-            }
-
+else {String  iL=ib.getText();
+          rodomod.display.setText(dt+iL);}
+        }
 }	
 @SuppressWarnings("unused")
 public static void main(String[] args) {
       Calc calc = new Calc();
-    }
+        }
  }
